fecdee8798a63ddfa866a68830efe43e1e4146ae
data-miner00 <data-miner00@users.noreply.github.com>
2020-10-29 01:55:56 +0800

    Rearrange methods and added comments

--- controller/controller.py
+++ controller/controller.py
@@ -3,23 +3,22 @@ from model.vocabulary import Vocabulary
 
 class Controller:
 
+
 	def __init__(self):
 		pass
 
+	# ( Main methods
+	# ========================================= #
+	# Creating or appending to a file
+	def add_word(self, language: str, word: str, meaning: str):
+			path = "database/" + language + ".txt"
 
-	def load_lang(self) -> dict:
-		with open("model/languages.json", "r") as lang_json:
-			languages = json.load(lang_json)
+			with open(path, "a+", encoding="utf-8") as f:
+				f.write(word + ":" + meaning + "\n")
 
-		return languages
+			print("Adding done")
 
-	def filter_lang(self, lang_input: str) -> str:
-		avail_lang = self.load_lang()
-		if lang_input in avail_lang:
-			return lang_input
-		else:
-			return "not found"
-
+	# Load dictionary into list of strings
 	def load_dict(self, language: str) -> [str]:
 		path = "database/" + language + ".txt"
 
@@ -32,12 +31,25 @@ class Controller:
 
 		return records
 
+	# ( Utility methods
+	# ========================================= #
+	# Load the language into dictionary
+	def load_lang(self) -> dict:
+		with open("model/languages.json", "r") as lang_json:
+			languages = json.load(lang_json)
+
+		return languages
+
+	# Filter the input for language
+	def filter_lang(self, lang_input: str) -> str:
+		avail_lang = self.load_lang()
+		if lang_input in avail_lang:
+			return lang_input
+		else:
+			return "not found"
+
+	
+
 		
-	def add_word(self, language: str, word: str, meaning: str):
-		path = "database/" + language + ".txt"
-
-		with open(path, "a+", encoding="utf-8") as f:
-			f.write(word + ":" + meaning + "\n")
-
-		print("Adding done")
+	
 	
--- view/interface.py
+++ view/interface.py
@@ -2,10 +2,13 @@ from controller.controller import Controller
 
 class ConsoleUI:
 
+	# Constructor
 	def __init__(self):
 		self.controller = Controller()
 	
-
+	# ( Display + get inputs
+	# ========================================= #
+	# Display screen with get selection
 	def default_screen(self) -> None:
 		print("""
 		|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|
@@ -18,6 +21,7 @@ class ConsoleUI:
 
 		return selection
 
+	# Display screen with get language
 	def select_lang(self) -> str:
 		lang_dict = self.controller.load_lang()
 		
@@ -29,31 +33,24 @@ class ConsoleUI:
 		lang_input = input("Language: ")
 		return lang_input
 
+	# ( Main methods
+	# ========================================= #
+	# Display the selected dictionary
 	def view_dict(self, language: str) -> None:
 		records = self.controller.load_dict(language)
 
 		for index, record in enumerate(records, start=0):
 			self.pretty_print(index, record)
 
-	def pretty_print(self, index: int, record: str):
-		info = record.split(":")
-		word = info[0]
-		meaning = info[1]
-		if (index == 0):
-			print("")
-			print("|================================================")
-		print("| " + str(index) + ". " +  word)
-		print("|================================================")
-		print("- " + meaning)
-		print("")
-
-
+	# Adding a vocabulary into file
 	def add_word(self, language: str) -> None:
 		word = input("Word: ")
 		meaning = input("Meaning: ")
 
 		self.controller.add_word(language, word, meaning)
 
+	# ( Initiate methods
+	# ========================================= #
 	def entry_point(self) -> None:
 		while True:
 			selection = self.default_screen()
